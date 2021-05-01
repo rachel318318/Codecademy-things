3f0b7160b2e52345219219c8d05a60c266d07f95
Rachel Lee <rachel318318@gmail.com>
2021-05-01 18:31:48 +0900

    introduction

--- US_Medical_Insurance/us_medical.py
+++ US_Medical_Insurance/us_medical.py
@@ -1,4 +1,26 @@
 # US Medical Insurance Costs project
-# Starting on April 17th, 2021
+# Starting on May 1st, 2021
 # Rachel Lee
 
+import pandas as pd
+
+df = pd.read_csv('insurance.csv')
+
+print(df.head())
+
+age = df['age']
+sex = df['sex']
+bmi = df['bmi']
+child = df['children']
+is_smoker = df['smoker']
+region = df['region']
+charges = df['charges']
+
+print(age.mean())
+num_female = sex[sex == 'female'].count()
+num_male = sex[sex == 'male'].count()
+
+female_percent = num_female/(num_male + num_female)
+male_percent = num_male/(num_male + num_female)
+
+print("asdf"[-2:])
